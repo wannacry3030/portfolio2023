0e6adc4cc96ab3d5d0b6904126241efd1661951e
wannacry3030 <wannacry.bat@proton.me>
2023-03-31 11:02:53 -0300

    att

--- main.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById("menu-toggle");
-const menu = document.getElementById("menu");
-
-menuToggle.addEventListener("click", () => {
-  menu.classList.toggle("show");
-});
