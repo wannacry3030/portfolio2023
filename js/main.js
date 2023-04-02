8bc3d577cad5bba1a8237394b6338de1807e2038
wannacry3030 <wannacry.bat@proton.me>
2023-04-02 11:09:45 -0300

    at

--- js/main.js
+++ js/main.js
@@ -33,3 +33,32 @@ window.addEventListener("load", () => {
   ps.forEach((p) => applyEffect(p));
   h2s.forEach((h2) => applyEffect(h2));
 });
+
+//POP UP
+// seleciona todos os quadrados
+const squares = document.querySelectorAll(".grid-item");
+
+// adiciona um evento de clique a cada quadrado
+squares.forEach((square) => {
+  square.addEventListener("click", () => {
+    // cria o elemento do pop-up
+    const popup = document.createElement("div");
+    popup.classList.add("popup");
+
+    // adiciona a descrição da tecnologia ao pop-up
+    const techName = square.querySelector("h2").textContent;
+    const techDescription = square.querySelector("p").textContent;
+    popup.innerHTML = `
+      <h2>${techName}</h2>
+      <p>${techDescription}</p>
+    `;
+
+    // adiciona o pop-up ao body
+    document.body.appendChild(popup);
+
+    // adiciona um evento de clique para fechar o pop-up
+    popup.addEventListener("click", () => {
+      popup.remove();
+    });
+  });
+});
